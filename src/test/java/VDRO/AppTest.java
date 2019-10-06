bb6182a1173342e3bdfc2214b0c81d734dd024a8
TomaszFaldrowicz <tomasz.faldrowicz>
2019-10-06 10:50:06 +0200

    first commit

--- src/test/java/VDRO/AppTest.java
+++ src/test/java/VDRO/AppTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple App.aa
  */
 public class AppTest 
     extends TestCase
