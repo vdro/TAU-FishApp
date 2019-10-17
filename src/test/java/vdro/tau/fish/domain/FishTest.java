83d413dba36f74b95346eb9dd76435c5140ed038
Tomasz Faldrowicz <Tomasz.faldrowicz@inkubit.com>
2019-10-17 17:38:52 +0200

    Test for fishNetPriceArgumentShouldAcceptOnlyPositiveNumberTest

--- src/test/java/vdro/tau/fish/domain/FishTest.java
+++ src/test/java/vdro/tau/fish/domain/FishTest.java
@@ -65,4 +65,19 @@ public void fishIsImplementedTest() {
                         true
                 );
         }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void fishNetPriceArgumentShouldAcceptOnlyPositiveNumberTest(){
+        new Fish(
+                1,
+                "TEST_Fish",
+                "Test Fish",
+                "This fish has been created in test class",
+                1,
+                1,
+                -1.20f,
+                1.60f,
+                true
+        );
+    }
         }
