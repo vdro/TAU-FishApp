b33e8c74ec478c92515a4bf449d104bc87d401b8
Tomasz Faldrowicz <Tomasz.faldrowicz@inkubit.com>
2019-10-17 17:36:54 +0200

    Test for fishQuantityArgumentShouldAcceptOnlyPositiveNumberTest

--- src/test/java/vdro/tau/fish/domain/FishTest.java
+++ src/test/java/vdro/tau/fish/domain/FishTest.java
@@ -51,4 +51,18 @@ public void fishIsImplementedTest() {
                         true
                 );
         }
+        @Test(expected = IllegalArgumentException.class)
+        public void fishQuantityArgumentShouldAcceptOnlyPositiveNumberTest(){
+                new Fish(
+                        1,
+                        "TEST_Fish",
+                        "Test Fish",
+                        "This fish has been created in test class",
+                        1,
+                        -1,
+                        1.20f,
+                        1.60f,
+                        true
+                );
+        }
         }
